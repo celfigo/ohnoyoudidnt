1693576f14ea85e450c2238439d36f9ee7c6e0a8
celfigo <scvural@gmail.com>
2017-05-23 16:51:50 +0300

    add "file" check for local html files

--- src/bg/background.js
+++ src/bg/background.js
@@ -20,9 +20,9 @@ function checkActive(tabs) {
         (function() {
             var thisTab = tabs[tabsLength];
 
-            //Only check tabs that have finished loading and are http / https
+            //Only check tabs that have finished loading and are http / https and local html files
             //This basically lets it ignore tabs like chrome://
-            if (thisTab.url.substring(0, 4) == "http" && thisTab.status == "complete") {
+            if ((thisTab.url.substring(0, 4) == "http" || thisTab.url.substring(0, 4) == "file" ) && thisTab.status == "complete") {
 
                 //Perform a no-op
                 chrome.tabs.executeScript(thisTab.id, {
